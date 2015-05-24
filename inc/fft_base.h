3844e56944d76cc9c5cf6758978c28c5679cc31c
Yudistira Virgus <yudistira.virgus@gmail.com>
2015-05-23 20:12:17 -0400

    * The destructor is the only method that even if it is pure virtual has to   have an implementation in order for the class it's defined in to be useful   (yes pure virtual functions can have implementations).

--- inc/fft_base.h
+++ inc/fft_base.h
@@ -1,18 +1,20 @@
-#ifndef _FFT_BASE
-#define _FFT_BASE
+#ifndef _FFT_HAO_BASE
+#define _FFT_HAO_BASE
 
 class FFTServer_base
 {
+ protected:
     int dimen;
     int *n;
     int L;
 
-    virtual ~FFTServer_base() = 0;
+ public:
+    virtual ~FFTServer_base() {}
 
     virtual std::complex<double>* fourier_forw(const std::complex<double>* inarray) = 0;
     virtual std::complex<double>* fourier_back(const std::complex<double>* inarray) = 0;
 
-    int const get_dimen const()
+    int const get_dimen() const
     {
         return dimen;
     }
@@ -22,7 +24,7 @@ class FFTServer_base
         return n;
     }
 
-    int const get_L const()
+    int const get_L() const
     {
         return L;
     }
